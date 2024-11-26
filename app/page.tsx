82b5327b09cf0814eb6888b51761fc1b97fc3e37
harish kumar <harishpathania150@gmail.com>
2024-11-26 19:02:54 +0530

    added some new lib

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
+// import Spline from "@splinetool/react-spline/next";
 
-
-export default function Home() {
-  return (
-    <div className="container mx-auto p-4">
-     
-     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-     <div className="bg-blue-200 text-white text-center rounded p-6">hey akshra</div>
-     <div className="bg-blue-200 text-white text-center rounded p-6">test 1</div>
-
-     </div>
-    </div>
-  )
-    
-}
+// export default function Home() {
+//   return (
+//     <div className="container mx-auto p-4">
+//       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+//         <div className="bg-blue-200 text-white text-center rounded p-6">
+//           hey akshra
+//         </div>
+//         <div className="bg-blue-200 text-white text-center rounded p-6">
+        
+//           <Spline scene="https://prod.spline.design/tSS4s3Zy839eX2DE/scene.splinecode" />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
